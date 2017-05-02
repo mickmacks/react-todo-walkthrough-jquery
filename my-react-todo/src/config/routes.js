7de2afbd30a9896b2624d02e6645a06ba38b45c2
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-05-02 10:11:35 -0700

    sprint 1 compleate. added Header route

---
+++ my-react-todo/src/config/routes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Route} from 'react-router'
+import App from '../App'
+
+
+module.exports = (
+	//  we will want to add '/todos'
+	<Route path='/' component={App}/>
+	)
