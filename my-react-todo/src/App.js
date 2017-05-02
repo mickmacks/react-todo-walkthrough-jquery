af0c1247d4c1bb7d2492c3001b236662d3c580c7
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-05-02 09:42:48 -0700

    Sprint 0 Complete: Working Structure

--- my-react-todo/src/App.js
+++ my-react-todo/src/App.js
@@ -1,12 +1,13 @@
-// src/App.js
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
         <h1>Hello World</h1>
       </div>
+      
     );
   }
 }
