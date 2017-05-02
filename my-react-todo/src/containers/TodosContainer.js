5e1e7df4359a3fe59be3b1556e274a87aef1bc52
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-05-02 15:17:00 -0700

    Sprint 4 Complete: Working create form

--- my-react-todo/src/containers/TodosContainer.js
+++ my-react-todo/src/containers/TodosContainer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import TodoModel from '../models/Todo'   // ajax GET statement
 import TodoList from '../components/TodoList' // gets array, adds ID and body
+import CreateTodoForm from '../components/CreateToDoForm.js'
 
 class TodosContainer extends Component {
 	constructor(){
@@ -19,11 +20,29 @@ class TodosContainer extends Component {
 			})
 		})
 	}
+
+	createTodo(newBody) {
+		
+		let newTodo = {
+			body: newBody,
+			completed: false
+		}
+		TodoModel.create(newTodo).then((res) => {
+			console.log('created todo', res)
+			let todos = this.state.todos
+			todos.push(res)
+			this.setState({todos: todos})
+		})
+
+	}
+
 	render() {
 
 		return (
 
 			<div className="todosContainer">
+				<CreateTodoForm 
+				 createTodo={this.createTodo.bind(this)}/>
 		        <TodoList
 		          todos={this.state.todos}
 		       	/>
